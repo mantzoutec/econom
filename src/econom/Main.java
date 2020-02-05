b70434a2d05362a528456f5a2dd95c7c63f50e6a
mantzoutec <>
2020-02-05 23:11:57 +0200

    test main change

--- src/econom/Main.java
+++ src/econom/Main.java
@@ -16,6 +16,11 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //
+        // Github test
+        //
+        //
+        
     }
     
 }
