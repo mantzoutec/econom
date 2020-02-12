1e793ed12a6635f041c0dc3f7b2f5fd291fffc8e
mantzoutec@gmail.com <mantzoutec@192.168.56.1>
2020-02-13 01:01:52 +0200

    Check test 202002130101

--- src/econom/Main.java
+++ src/econom/Main.java
@@ -16,15 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // test test
-        //
-        //  test
-        //
-        //
-        //
-        //
-        //
-        //
+       
         
     }
     
