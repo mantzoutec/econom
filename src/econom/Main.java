0a3a32f27e1112486cdd0f68abb2fd38c5119bfe
mantzoutec@gmail.com <kmt@TCB-LAPTOP1.tcb.local>
2020-02-15 20:30:40 +0200

    3GE libraries included

--- src/econom/Main.java
+++ src/econom/Main.java
@@ -5,22 +5,21 @@
  */
 package econom;
 
+
+
+
 /**
  *
- * @author Dell
+ * @authors
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        // test modify code 20200213.0109
-        
        
-         
+        
+        
+           
     }
     
 }
